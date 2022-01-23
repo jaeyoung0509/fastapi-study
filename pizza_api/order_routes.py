d0134bb9592158a751927dcacd5d75e056db6235
jaeyoung0509 <jaeyoung0509@naver.com>
2022-01-23 21:48:18 +0900

    pizza_api  commit

---
+++ pizza_api/order_routes.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+order_router = APIRouter(
+    prefix='/orders',
+    tags = ['orders']
+)
+
+@order_router.get('/')
+async def hello():
+    return {"message" :"hello world"}
