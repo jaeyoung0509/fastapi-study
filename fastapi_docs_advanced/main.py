6011556bec568353d1f0fd8cfd75d8dc3f6f8e2a
jaeyoung0509 <jaeyoung0509@naver.com>
2022-02-13 15:46:05 +0900

    about depends

--- fastapi_docs_advanced/main.py
+++ fastapi_docs_advanced/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI , Response
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI , Response , Depends
 app = FastAPI()
 
 
@@ -15,3 +14,32 @@ cookie-and-object
 async def create_cookie(response : Response):
     response.set_cookie(key="fakesession" , value="fake-cookie-session-value")
     return {"message"  : " i have cookies"}
+
+
+'''
+response Headers
+get_headers
+'''
+@app.get("/headers-and-objects")
+async def get_headers(response:  Response):
+    response.headers["X-Cat-dog"] ="hi"
+    return {"message": "Hello World"}
+
+
+'''
+Advanced Dependencies
+''' 
+class FixedContentQueryChecker:
+    def __init__(self , fixed_content :str) -> None:
+        self.fixed_content = fixed_content
+    
+    def __call__(self, q: str = "bar") -> False:
+        if q:
+            return self.fixed_content in q
+
+checker = FixedContentQueryChecker("bar")
+
+@app.get('/query-checker')
+async def read_query_check(fixed_content_included : bool = Depends(checker)):
+    
+    return {"fixed_content_in_query" : fixed_content_included}
